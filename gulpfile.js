ffaef8e58f8dca8ca06b09d89471ea5bb2a2ad6c
acrifolium <olivier.azevedo@gmail.com>
2017-02-09 23:01:23 +0100

    restructuration project first step

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,10 @@
-var gulp = require('gulp');
-var tasks = './gulp/tasks/';
+let gulp = require('gulp');
 
-gulp.task("clean", require(tasks + "clean"))
+const import serve from './tasks/serve';
+const import build from './tasks/build';
+const import api from './tasks/api';
 
-// install bower
-gulp.task("bower:install", require(tasks + "bower"))
-
-// static assets
-gulp.task("public", require(tasks + "public"))
-gulp.task("assets", require(tasks + "assets"))
-
-// generated assets
-gulp.task("scripts", require(tasks + "scripts")) 
-
-// generated css
-gulp.task("css", require(tasks + "/stylesheets"))
-
-// inject
-///gulp.task("inject", ["scripts", "stylesheets"], require(tasks + "/inject"))
-///gulp.task("inject:all", ["combine"], require(tasks + "/inject"))
-
-// build
-gulp.task("dist", [
-  "clean",
-  "bower:install",
-
-  "public",
-  "assets",
-
-  "scripts",
-  "css",
-])
-
-// Init
-gulp.task("init", [
-  "clean",
-  "bower:install"
-])
-
-// dev tasks
-gulp.task("watch", require(tasks + "watch"))
-gulp.task("watch:all", ["dist"], require(tasks + "watch"))
-gulp.task("default", [
-  "dist",
-  "watch"
-])
+gulp.task('serve', serve);
+gulp.task('build', build);
+gulp.task('api', api);
+gulp.task('default', ['serve']);
